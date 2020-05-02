1c2ac546a7feaac939cd48360c27659896e56bb5
Lubomír Nedbal <lopliok@Lopliok-Mac-Pro.local>
2020-05-02 15:25:59 +0200

    test

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+const PORT = process.env.PORT || '5000';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,6 @@ async function bootstrap() {
   });
   app.use(cookieParser());
 
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
